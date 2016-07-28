953780fd8ece9f01fbe6b81ecb9756630a985b1f
irworks <admin@irworks.de>
2016-07-28 14:45:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by irworks on 28.07.16.
+ * © Copyright irworks, 2016
+ * All rights reserved, do not distribute
+ */
+
+/**
+ * Module: Application startpoint
+ * File: v2/index.php
+ * Depends: config
+ */
+
+
+
+?>
