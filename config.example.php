fe228c89092fe04441c175665797b2af73d46e30
irworks <admin@irworks.de>
2016-07-29 00:09:26 +0200

    + Added example config

---
+++ config.example.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by irworks on 29.07.16.
+ * © Copyright irworks, 2016
+ * All rights reserved, do not distribute
+ */
+
+/**
+ * Module: Example config
+ * File: v2/config.example.php
+ * Depends: [NONE]
+ */
+
+/**
+ *  |----------------------------------------------|
+ *  |                                              |
+ *  |                twote API v2                  |
+ *  |     ~ Rename this file to config.php ~       |
+ *  |                                              |
+ *  |----------------------------------------------|
+ */
+
+
+/* MySQL Settings */
+define('MYSQL_HOST',     'localhost');
+define('MYSQL_USERNAME', '?????????');
+define('MYSQL_PASSWORD', '?????????');
+define('MYSQL_DB',       '?????????');
+
+/* API Settings */
+define('DEFAULT_LANG', 'en');
+?>
